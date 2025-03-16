43b542b76d3cc8782d4023a18b362eddc3a7c146
Lucas Calil <calil.lucas@outlook.com>
2025-03-16 00:10:46 -0300

    Alterações de aparência em html e css

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Variáveis de Início://
+let nomesArray = [];
+
+//Função para adicionar os nomes ao array://
+function adicionarAmigo() {
+    let inputNome = document.getElementById('inputNome');
+    let nome = inputNome.value;
+
+    if (nome === "") {
+        alert('Por favor, insira um nome válido.');
+        return;
+    }
+
+    nomesArray.push(nome);
+    inputNome.value = "";
+    inputNome.focus();
+
+    let listaNomes = document.getElementById('listaNomes');
+    listaNomes.innerHTML = "";
+
+    for (let i = 0; i < nomesArray.length; i++) {
+        listaNomes.innerHTML += "<li>" + nomesArray[i] + "</li>";
+    }
+}
